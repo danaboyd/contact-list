6b540b888d4d8bb14668cd1d96f408d343203f65
TangyrenMidKing <65498450+TangyrenMidKing@users.noreply.github.com>
2021-05-21 19:13:21 -0400

    Add files via upload

---
+++ js/new.js
@@ -0,0 +1,11 @@
+function  showSignIn()
+{
+  document.getElementById("sign_in").style.display='inline-block';
+  document.getElementById("sign_up").style.display='none';
+}
+
+function showSignUp()
+{
+  document.getElementById("sign_up").style.display='inline-block';
+  document.getElementById('sign_in').style.display='none';
+}
